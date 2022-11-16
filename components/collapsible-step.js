dda9ea9da4719f63910da29f8ad0168befd298a7
Walter Kimaro <wakyj07@gmail.com>
2022-11-16 22:24:14 +0300

    UI tweaks

--- components/collapsible-step.js
+++ components/collapsible-step.js
@@ -2,10 +2,12 @@ export default function CollapsibleStep() {
 	return {
 		restrict: "E",
 		transclude: true,
-		scope: { step: "@", heading: "@", expanded: "=", isLast: "@" },
-		link: (scope) => {
-			// scope.expanded = scope.expanded && scope.expanded != "false";
-			console.log("Collapsed: ", scope.expanded, typeof scope.expanded);
+		scope: {
+			step: "@",
+			heading: "@",
+			expanded: "=",
+			completed: "=",
+			isLast: "@",
 		},
 		replace: true,
 		template: /*html*/ `
@@ -15,12 +17,16 @@ export default function CollapsibleStep() {
             ></div>
             <div class="flex py-5">
                 <div
-                    class="rounded-full w-6 h-6 text-sm flex items-center justify-center text-white mr-5 relative z-10"
-                    ng-class="{'bg-neutral-400': !expanded, 'bg-accent': expanded}"
+                    class="flex-shrink-0 rounded-full w-6 h-6 text-sm flex items-center justify-center text-white mr-5 relative z-10"
+                    ng-class="{'bg-neutral-400': !expanded && !completed, 'bg-accent': expanded || completed}"
                 >
-                    {{step}}
+                    <span ng-show="!completed || expanded">{{step}}</span>
+                    <svg ng-show="completed && !expanded" class="w-3 h-3" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                        <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                    </svg>
                 </div>
-                <div class="flex-1">
+                <div class="flex-1 min-w-0">
                     <h3 class="text-lg leading-none font-medium mt-1 opacity-60">
                         {{heading}}
                     </h3>
--- pages/import/controller.js
+++ pages/import/controller.js
@@ -8,11 +8,12 @@ export default function ImportController($scope, $routeParams, ImportService) {
 		"Phone Number",
 		"Dept",
 	];
-	$scope.importActions = [
-		"Only add new data",
-		"Only update existing data",
-		"Add new data and update existing",
-	];
+	$scope.importActions = {
+		"Only add new data": "add",
+		"Only update existing data": "update",
+		"Add new data and update existing": "both",
+	};
+	$scope.importActionsChoices = Object.keys($scope.importActions);
 	$scope.decoArray = Array(300).fill(Math.random());
 	$scope.importFromChoices = {
 		"Upload file": "file",
@@ -20,13 +21,21 @@ export default function ImportController($scope, $routeParams, ImportService) {
 		"Copy / Paste from Excel": "excel",
 	};
 	$scope.importFromKeys = Object.keys($scope.importFromChoices);
+	$scope.completedSteps = [];
+	$scope.invalidRows = ["Lead method actor", "Head of extras"];
 
 	$scope.vm = {
 		data: null,
 		importFrom: "file",
 		currentStep: 1,
-		importAction: "",
-		importType: "",
+		importAction: "add",
+		importType: "Crew",
+		dataFixColumn: "Position",
 		columnMap: {},
 	};
+	$scope.$watch("vm.currentStep", function (newValue) {
+		if (!$scope.completedSteps.includes(newValue)) {
+			$scope.completedSteps.push(newValue - 1);
+		}
+	});
 }
