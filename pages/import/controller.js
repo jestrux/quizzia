cbe4899ad252ab7534975594923788bc5d0b4adb
Walter Kimaro <wakyj07@gmail.com>
2022-11-16 14:53:15 +0300

    Add empty state and more import from options

--- pages/import/controller.js
+++ pages/import/controller.js
@@ -14,8 +14,16 @@ export default function ImportController($scope, $routeParams, ImportService) {
 		"Add new data and update existing",
 	];
 	$scope.decoArray = Array(300).fill(Math.random());
+	$scope.importFromChoices = {
+		"Upload file": "file",
+		"Google Sheets": "google",
+		"Copy / Paste from Excel": "excel",
+	};
+	$scope.importFromKeys = Object.keys($scope.importFromChoices);
 
 	$scope.vm = {
+		data: null,
+		importFrom: "file",
 		currentStep: 1,
 		importAction: "",
 		importType: "",
